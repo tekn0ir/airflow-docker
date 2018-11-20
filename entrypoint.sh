dfc3b10c827a1c612d830c3aabec7a3ea8182583
Anders Åslund <anders.aslund@klarna.com>
2018-11-20 18:19:27 +0100

    Speeding up startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -xe
 
-TRY_LOOP="20"
+TRY_LOOP="60"
 
 AIRFLOW__CORE__FERNET_KEY=${FERNET_KEY:=$(python3.6 -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)")}
 
@@ -9,14 +9,14 @@ if [[ $POSTGRES_ENABLED ]]; then
     wait_for_port() {
       local name="$1" host="$2" port="$3"
       local j=0
-      while ! nc -z "$host" "$port" >/dev/null 2>&1 < /dev/null; do
+      while ! nc -z -w 4 "$host" "$port" >/dev/null 2>&1 < /dev/null; do
         j=$((j+1))
         if [ $j -ge $TRY_LOOP ]; then
           echo >&2 "$(date) - $host:$port still not reachable, giving up"
           exit 1
         fi
         echo "$(date) - waiting for $name($host:$port)... $j/$TRY_LOOP"
-        sleep 5
+        sleep 1
       done
     }
 
