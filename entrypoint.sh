320c736bfa9a10b4d391d44c2c676fc6e6b62cd9
Anders Åslund <anders.aslund@klarna.com>
2018-11-20 19:12:49 +0100

    Upgrade to Airflow 1.10.1rc2

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,21 +5,21 @@ TRY_LOOP="60"
 
 AIRFLOW__CORE__FERNET_KEY=${FERNET_KEY:=$(python3.6 -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)")}
 
-if [[ $POSTGRES_ENABLED ]]; then
-    wait_for_port() {
-      local name="$1" host="$2" port="$3"
-      local j=0
-      while ! nc -z -w 4 "$host" "$port" >/dev/null 2>&1 < /dev/null; do
-        j=$((j+1))
-        if [ $j -ge $TRY_LOOP ]; then
-          echo >&2 "$(date) - $host:$port still not reachable, giving up"
-          exit 1
-        fi
-        echo "$(date) - waiting for $name($host:$port)... $j/$TRY_LOOP"
-        sleep 1
-      done
-    }
+wait_for_port() {
+  local name="$1" host="$2" port="$3"
+  local j=0
+  while ! nc -z -w 4 "$host" "$port" >/dev/null 2>&1 < /dev/null; do
+    j=$((j+1))
+    if [ $j -ge $TRY_LOOP ]; then
+      echo >&2 "$(date) - $host:$port still not reachable, giving up"
+      exit 1
+    fi
+    echo "$(date) - waiting for $name($host:$port)... $j/$TRY_LOOP"
+    sleep 1
+  done
+}
 
+if [[ $POSTGRES_ENABLED ]]; then
     export SQL_ALCHEMY_CONN="postgresql+psycopg2://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB"
     wait_for_port "Postgres" "$POSTGRES_HOST" "$POSTGRES_PORT"
 fi
